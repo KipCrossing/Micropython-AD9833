6da32064d39cb677d4aa933efbf585056a7655bc
kip <kip.crossing@gmail.com>
2017-03-14 15:13:18 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,93 @@
+'''
+Written by Kipling Crossing
+This sample code was written in python 3 for micropython
+for interfacing between the Pyboard (or other) and the 
+AD9833 over SPI. 
+
+This code was inspired by Arduion code by sinneb
+www.sinneb.net/?p=562
+
+The connections between the AD9833 and PyBoard are:
+
+VCC -> Vin (any)
+DGND -> GND (any)
+SDATA -> MOSI (X8)
+SCLK -> SCK (X6)
+FSYNC -> ss (X5)
+AGND -> GND (any)
+
+
+'''
+
+from pyb import Pin
+from pyb import SPI
+from pyb import ADC
+import time
+
+# Define frequency in Hz
+Freq =10000  
+
+# Obtain Clock Frequency        #Note: this method may need to be checked as freq does not match output frequency
+(x,x,ClockFreq,x) = pyb.freq()
+
+# Calculate frequency word to send
+word = hex(round((Freq*2**28)/ClockFreq))
+
+# Split frequency word onto its seperate bytes
+MSB = (int(word) & 0xFFFC000)>>14
+LSB = int(word) & 0x3FFF
+
+
+# Set control bits DB15 = 0 and DB14 = 1; for frequency register 0
+MSB |= 0x4000
+LSB |= 0x4000
+
+# Allocate X5 as ss for FSYNC
+p_out = Pin('X5', Pin.OUT_PP)
+
+# Initiate SPI bus
+spi = SPI(1, SPI.MASTER, baudrate=9600, polarity=1, phase=0,firstbit=SPI.MSB)
+
+#function for splitting hex into high and low bits
+def bytes(integer):
+    return divmod(integer, 0x100)
+
+# This is the function that sends the data
+def Send(data):
+    high, low = bytes(data)
+    p_out.low()
+
+    spi.send(high)
+    spi.send(low)
+
+    p_out.high()
+
+
+Send(0x2100)
+
+#Set the frequency
+Send(LSB)    #lower 14 bits
+Send(MSB)    #Upper 14 bits
+
+#phase
+Send(0&0xC000)
+#shape
+Send(0x2000)    # square: 0x2020, sin: 0x2000, triangle: 0x2002
+
+
+'''
+adc = ADC(Pin('X19'))
+while True:
+    print(adc.read())     # read value, 0-4095
+
+'''
+# For observing the wave using pin X19 ADC
+adc = pyb.ADC(pyb.Pin.board.X19)    # create an ADC on pin X19
+buf = bytearray(1000)                # create a buffer of 100 bytes
+adc.read_timed(buf, 1000000)             # read analog values into buf at 10Hz
+                                    #   this will take 10 seconds to finish
+for val in buf:                     # loop over all values
+    print(val)                      # print the value out
+
+
+
